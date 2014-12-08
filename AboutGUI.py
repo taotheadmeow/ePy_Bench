066558afde8db1cd0b47c530bbd602736b615b85
it57070044 <s7070044@kmitl.ac.th>
2014-12-08 12:01:22 +0700

    add message
    name of producer

--- AboutGUI.py
+++ AboutGUI.py
@@ -1,3 +1,7 @@
 """About"""
 from Tkinter import *
 import tkMessageBox
+tkMessageBox.showinfo("This is the name of producers",
+                      "Mr.Tanakrit Tangdamrongsap 57070044\nMr.Sakan")
+root = Tk()
+root.mainloop()
