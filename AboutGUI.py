060a89bf19a82c03229b668e96567ed9a3888abc
it57070044 <s7070044@kmitl.ac.th>
2014-12-08 12:21:37 +0700

    Add detail
    How to use

--- AboutGUI.py
+++ AboutGUI.py
@@ -1,7 +1,17 @@
-"""About"""
+"""About_GUI"""
 from Tkinter import *
 import tkMessageBox
-tkMessageBox.showinfo("This is the name of producers",
-                      "Mr.Tanakrit Tangdamrongsap 57070044\nMr.Sakan")
+"""Use for show detail of program and  the name of producers"""
+tkMessageBox.showinfo("This is the name of producers and detail of this program",
+                      "Mr.Tanakrit Tangdamrongsap 57070044\n"
+                      "Mr.Sakan \n\n"
+                      "This program is benchmark.\n"
+                      "Use for speed test of your computer by test with runtime of e value. \n\n"
+                      "How to use \n\n"
+                      "1.Select the number of digit of e value. \n"
+                      "2.Select multicore processing if you want. \n"
+                      "3.Press 'Start' button. \n"
+                      "4.If process is too long you can press 'Force Stop' to stop the process. \n"
+                      "5.Check your computer speed.")
 root = Tk()
 root.mainloop()
