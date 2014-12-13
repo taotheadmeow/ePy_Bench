6ff6e415689a154b5b9d3a2cec5f38ed4a51d221
Sakan Promlainak <s7070110@kmitl.ac.th>
2014-12-13 17:47:10 +0700

    Infomation update

--- AboutGUI.py
+++ AboutGUI.py
@@ -8,8 +8,8 @@ import tkMessageBox
 def about_pop():
     tkMessageBox.showinfo("About ePy Bench",
                           "Developed by \n"
-                          "Mr.Tanakrit Tangdamrongsap 57070044 \n"
-                          "Mr.Sakan Promlainak 57070110 \n\n"
+                          "    Mr.Tanakrit Tangdamrongsap \t(Student no. 57070044) \n"
+                          "    Mr.Sakan Promlainak \t\t(Student no. 57070110) \n\n"
                           "This program is for benchmarking and performance testing.\n"
                           "Use for testing speed of your computer by test with runtime of e value. \n\n"
                           "If you found bug... sent report to s7070110@kmitl.ac.th."
@@ -18,11 +18,11 @@ def about_pop():
 def help_pop():
     tkMessageBox.showinfo("Help",
                           "How to use \n"
-                          "1. Select the number of digit of e value. \n"
-                          "2. Select multicore processing if you want. \n"
-                          "3. Press 'Start' button. \n"
-                          "4. Wait for result. \n"
-                          "5. Check your computer speed.\n\n"
+                          "    1. Select the number of digit of e value. \n"
+                          "    2. Select multicore processing if you want. \n"
+                          "    3. Press 'Start' button. \n"
+                          "    4. Wait for result. \n"
+                          "    5. Check your computer speed.\n\n"
                           "ps. If process is too long you can press 'Force Stop' to stop the process."
                           )
 
